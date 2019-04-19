db4b7e15df568a706af5565c89a02b7084935936
nurennafisa <30334024+nurennafisa@users.noreply.github.com>
2019-04-19 15:24:55 +0600

    Add files via upload

---
+++ eCOMMANDO/register.php
@@ -0,0 +1,50 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration system PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Register</h2>
+	</div>
+	
+	<form method="post" action="register.php">
+
+		<?php include('errors.php'); ?>
+      
+			<div class="input-group">
+			<label>NID/Birth_Certificate_ID</label>
+			<input type="text" name="id" value="<?php echo $id; ?>">
+		</div>
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="name" value="<?php echo $name; ?>">
+		</div>
+		<div class="input-group">
+			<label>Phone</label>
+			<input type="number" name="phone" value="<?php echo $phone; ?>">
+		</div>
+		
+		<div class="input-group">
+			<label>Email</label>
+			<input type="email" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		<div class="input-group">
+			<label>Confirm password</label>
+			<input type="password" name="password_2">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="reg_user">Register</button>
+		</div>
+		<p>
+			Already a member? <a href="login.php">Sign in</a>
+		</p>
+	</form>
+</body>
+</html>
