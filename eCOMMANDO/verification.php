b8be0a5a0934f2a4b83da093b4aa43ce5b83d293
nurennafisa <30334024+nurennafisa@users.noreply.github.com>
2019-04-19 23:28:15 +0600

    Add files via upload

--- eCOMMANDO/verification.php
+++ eCOMMANDO/verification.php
@@ -1,25 +1,32 @@
-<?php
+<?php 
+if(!isset($_SESSION)){
+    session_start();
+}
+		
+
+
+
 $con = mysql_connect("localhost","","");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }  
 mysql_select_db("ecommando", $con);
+$email=$_SESSION['email'];
 
 $result = mysql_query("SELECT * FROM subadmin_info
                        
 						");
 						
-
-   $flag=0;
+$_SESSION['recentid'] = $_POST['id'];
+ 
 while($row = mysql_fetch_array($result))
   {
- 
-   if('$_POST[id]'!=$row['NID'] and '$_POST[password]'!=$row['password'] )
-   header('location: subadminlogin.php');
-   
-   }  
- header('location: userlogin.php');   
+ if(($_POST['id']==$row['NID']) and ($_POST['password']==$row['password']))
+ {
+      header('location: subadminlogin.php');
+   }
+   }
  
 	
  
